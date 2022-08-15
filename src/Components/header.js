57ca4726e698967beb32878afa8c6e817f3cb7b2
Sakshi Nayan <sakshi12princi@gmail.com>
2022-08-15 21:57:43 +0530

    day2

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,13 +1,12 @@
 import React from 'react'
  import './header.css'
 const Header=()=> {
-    //const Authtoken=localStorage.getItem("authorization");
-    
+    const Authtoken=localStorage.getItem("authorization");
     const username = localStorage.getItem("userName");
   return (
     <>
       <div id="header1">
-        <p className='paraUser'>{username}</p>
+        <p className='paraUser'>{username}User Name</p>
         
       </div>
     </>
